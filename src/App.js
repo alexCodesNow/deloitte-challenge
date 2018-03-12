608884f3be9d3d423fa567a3e3e3a29f6959dcba
AlexShp <alex.shparev@gmail.com>
2018-03-12 22:39:27 +0100

    wip: Added Bootstrap, Less; moved assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
+        <div className="app-header">
+
+          <div className="app-header-title">
+          Discovering beer just got easier
+          </div>
+          <button type="button" className="app-header-button btn btn-primary">
+            Learn More
+          </button>
+
+        </div>
+        <div className="app-body"></div>
+        <div className="app-footer"></div>
       </div>
     );
   }
