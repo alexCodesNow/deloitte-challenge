40ad991500b6d331aeed69e51ca0787127eec6c2
AlexShp <alex.shparev@gmail.com>
2018-03-17 20:48:07 +0100

    Basic alignment, content getting filled in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button } from 'bootstrap';
+import * as pageContent from './constants.js';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -8,16 +9,65 @@ class App extends Component {
       <div className="app">
         <div className="app-header">
 
+          <header>
+            <div className="header-icon"></div>
+
+            <nav>
+              <ul>
+                <li>Beers</li>
+                <li>Breweries</li>
+                <li>Submit a Find</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          </header>
+
           <div className="app-header-title">
-          Discovering beer just got easier
+            Discovering beer<br/>just got easier
           </div>
           <button type="button" className="app-header-button btn btn-primary">
             Learn More
           </button>
 
         </div>
-        <div className="app-body"></div>
-        <div className="app-footer"></div>
+
+        <div className="app-body">
+          <div class="row container">
+            <div className="get-beer row">
+              <div className="get-beer-info col-6">
+                <div className="get-beer-info-1">{pageContent.BEER_INFO_TITLE}</div>
+                <div className="get-beer-info-2">{pageContent.BEER_INFO_1}<br/>{pageContent.BEER_INFO_2}</div>
+              </div>
+              <form className="col-6">
+                <label>
+                  Name <input type="text" name="name" />
+                </label>
+                <input type="submit" value="Submit" />
+              </form>
+            </div>
+          </div>
+
+
+          <div className="about-us">
+            <h1>What makes us different?</h1>
+            <h2>WE’RE FOCUSED ON THE NEEDS OF BEER LOVERS.</h2>
+            <div className="about-us-detail">
+              <div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="app-footer">
+          <footer>
+            <div className="footer-address">
+              12345 Fremont Ave., Suite 1001,<br/> Seattle, WA 98100
+            </div>
+            <div className="footer-copyright">Copyright &copy; 2013 Beer’d, Inc</div>
+          </footer>
+        </div>
       </div>
     );
   }
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+
+export const BEER_INFO_TITLE = "You’re a couple of moments away from getting a beer.";
+export const BEER_INFO_1 = "Let’s start with some basics. By letting us know a bit about your tastebuds, we’ll be able to start gathering the information we need in order to get you a beer. We may even be able to introduce you to something you’ve never tried before but will love.";
+export const BEER_INFO_2 = "We’re a company who likes beer. We help beer lovers like you find your favorite beers and explore new brews, without the hassle and wait times of your local pub. And since we sell beer directly, we also save you money.";
